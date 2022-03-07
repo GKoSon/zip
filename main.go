5258172b24fd5f96ae7fe3c89504eeb64fea4c3f
KOSON <18818209416@139.com>
2022-03-07 11:19:24 +0800

    整理上位机下位机2个exe-分别build即可

--- main.go
+++ main.go
@@ -11,14 +11,22 @@ func main() {
 
 	/*俄罗斯套娃做法*/
 	/*正向*/
+
 	zip.ZipDir("./.git", "./TEMP.ZIP")
 	zip.ZipFile("./TEMP.ZIP", "./IMG.ZIP", "IMG") //把压缩包TEMP.ZIP再次压缩 压后名字是IMG.ZIP 它里面有一个文件+一个MD5文件 其实这个文件是一个压缩包
 	os.Remove("./TEMP.ZIP")
 	fmt.Println("DONE")
+
 	/*逆向*/
 	/*
 		zip.UNZipFile("./IMG.ZIP") //解压 出来一个文件夹 名字是上面的参数3 "IMG"
-		fmt.Println(zip.CheckFileMd5("./IMG/IMG", "./IMG/IMG.md5"))
-		zip.UNZipDir("./IMG/IMG", "./IMG/.git")
+		if zip.CheckFileMd5("./IMG/IMG", "./IMG/IMG.md5") {
+			zip.UNZipDir("./IMG/IMG", "./IMG/.git")
+			os.Remove("./IMG/IMG")
+			os.Remove("./IMG/IMG.md5")
+			fmt.Println("DONE")
+		} else {
+			fmt.Println("FAIL")
+		}
 	*/
 }
